172123dad431e1cca4f6b8b9af30f9f374a7644d
Ilcho Vuchkov <ilcho.vuchkov@ffwagency.com>
2022-05-19 09:12:27 +0300

    Update delete photo messenger.

--- src/MessageHandler/DeleteImagePostHandler.php
+++ src/MessageHandler/DeleteImagePostHandler.php
@@ -3,30 +3,34 @@
 namespace App\MessageHandler;
 
 use App\Message\DeleteImagePost;
+use App\Message\DeletePhotoFile;
 use App\Photo\PhotoFileManager;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Validator\Tests\Fixtures\Entity;
 
 class DeleteImagePostHandler implements MessageHandlerInterface
 {
 
-    private PhotoFileManager $photoManager;
+    private MessageBusInterface $messageBus;
     private EntityManager $entityManager;
 
-    public function __construct(PhotoFileManager $photoManager, EntityManager $entityManager)
+    public function __construct(MessageBusInterface $messageBus, EntityManager $entityManager)
     {
-        $this->photoManager = $photoManager;
+        $this->messageBus = $messageBus;
         $this->entityManager = $entityManager;
     }
 
     public function __invoke(DeleteImagePost $deleteImagePost)
     {
         $imagePost = $deleteImagePost->getImagePost();
-        $this->photoManager->deleteImage($imagePost->getFilename());
+        $filename = $imagePost->getFilename();
 
         $this->entityManager->remove($imagePost);
         $this->entityManager->flush();
+
+        $this->messageBus->dispatch(new DeletePhotoFile($filename));
     }
 
 }
