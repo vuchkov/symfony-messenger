bc928d63d0b16b1e2741a9816a61a7585ef81ff3
Ilcho Vuchkov <ilcho.vuchkov@ffwagency.com>
2022-05-19 15:13:31 +0300

    Updat emessenger.

---
+++ src/Messenger/UniqueIdStamp.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Messenger;
+
+use Symfony\Component\Messenger\Stamp\StampInterface;
+
+class UniqueIdStamp implements StampInterface
+{
+    private $uniqueId;
+
+    public function __construct()
+    {
+        $this->uniqueId = uniqid('', true);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUniqueId(): string
+    {
+        return $this->uniqueId;
+    }
+
+}
