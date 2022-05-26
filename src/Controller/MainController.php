8c1537357818441ad82582322644160a703c0139
Ilcho Vuchkov <ilcho.vuchkov@ffwagency.com>
2022-05-26 11:26:51 +0300

    Add message query bus - show images count.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Message\Query\GetTotalImageCount;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,8 +13,15 @@ class MainController extends AbstractController
     /**
      * @Route("/")
      */
-    public function homepage()
+    public function homepage(MessageBusInterface $queryBus)
     {
-        return $this->render('main/homepage.html.twig');
+        $envelop = $queryBus->dispatch(new GetTotalImageCount());
+        /** @var HandledStamp $handled */
+        $handled = $envelop->last(HandledStamp::class);
+        $imageCount = $handled->getResult();
+
+        return $this->render('main/homepage.html.twig', [
+            'imageCount' => $imageCount,
+        ]);
     }
 }
